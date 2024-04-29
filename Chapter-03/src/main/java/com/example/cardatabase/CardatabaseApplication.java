4c54edfc14dfb2ddf92d1047bf6d82c6649854d3
João Pedro Nolasco <nolascojoaops@gmail.com>
2024-04-29 11:17:28 -0300

    CH-03: Add example for create and access db with jpa

---
+++ Chapter-03/src/main/java/com/example/cardatabase/CardatabaseApplication.java
@@ -0,0 +1,50 @@
+package com.example.cardatabase;
+
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.cardatabase.domain.Car;
+import com.example.cardatabase.domain.CarRepository;
+import com.example.cardatabase.domain.Owner;
+import com.example.cardatabase.domain.OwnerRepository;
+
+@SpringBootApplication
+public class CardatabaseApplication implements CommandLineRunner {
+
+	private static final Logger logger = LoggerFactory.getLogger(CardatabaseApplication.class);
+
+	private final CarRepository repository;
+	private final OwnerRepository orepository;
+
+	public CardatabaseApplication(CarRepository repository, OwnerRepository orepository) {
+		this.repository = repository;
+		this.orepository = orepository;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardatabaseApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Owner owner1 = new Owner("John", "Johnson");
+		Owner owner2 = new Owner("Mary", "Robinson");
+		orepository.saveAll(Arrays.asList(owner1, owner2));
+		
+		repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2023, 59000, owner1));
+		repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2020, 29000, owner2));
+		repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2022, 39000, owner2));
+
+		for (Car car : repository.findAll()) {
+			logger.info("band: {}, model: {}", car.getBrand(), car.getModel());
+		}
+
+	}
+
+}
