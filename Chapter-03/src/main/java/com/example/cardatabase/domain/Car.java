39c071da5c7ce1ef10135b9d783f81f46f954946
João Pedro Nolasco <nolascojoaops@gmail.com>
2024-04-29 17:37:00 -0300

    CH-03: Switched from GenerationType.AUTO to IDENTITY to prevent MariaDB log errors

--- Chapter-03/src/main/java/com/example/cardatabase/domain/Car.java
+++ Chapter-03/src/main/java/com/example/cardatabase/domain/Car.java
@@ -12,7 +12,7 @@ import jakarta.persistence.ManyToOne;
 public class Car {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String brand, model, color, registrationNumber;
 	private int modelYear, price;
--- Chapter-03/src/main/java/com/example/cardatabase/domain/Owner.java
+++ Chapter-03/src/main/java/com/example/cardatabase/domain/Owner.java
@@ -13,7 +13,7 @@ import jakarta.persistence.OneToMany;
 public class Owner {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long ownerid;
 	private String firstname;
 	private String lastname;
